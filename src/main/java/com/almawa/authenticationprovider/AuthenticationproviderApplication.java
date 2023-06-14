dd62b278293e9f3512d9ba338a2a42d9ad5b84ab
Daniel Almanza <daniel.almanzawagner@gmail.com>
2023-06-14 11:39:19 +0200

    first commit

---
+++ src/main/java/com/almawa/authenticationprovider/AuthenticationproviderApplication.java
@@ -0,0 +1,13 @@
+package com.almawa.authenticationprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationproviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almawa/authenticationprovider/AuthenticationproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almawa.authenticationprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
